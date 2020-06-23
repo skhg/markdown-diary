ab0207712c53007480b37b919b4d8ecb2b755cff
Jack <git@jackhiggins.ie>
2020-06-23 16:55:45 +0200

    preparation stuff for publishing on pypi

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='markdown-diary',
+    version='0.0.0',
+    description="CLI tool to generate monthly diary files in simple Markdown format",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='diary journal markdown cli',
+    author='Jack Higgins',
+    author_email='pypi@jackhiggins.ie',
+    url='https://github.com/skhg/py_md_diary',
+    packages=['py_md_diary'],
+    install_requires=[
+    ],
+    tests_require=[
+        'mock',
+        'nose',
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'])
